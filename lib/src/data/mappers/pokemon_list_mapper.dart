c28de3b388bee64ba75b80f3013fe9d538deb43c
CaioWF <contato.caioweliton@gmail.com>
2024-09-08 18:51:16 -0300

    feat: :sparkles: add string util & improve mapper

--- lib/src/data/mappers/pokemon_list_mapper.dart
+++ lib/src/data/mappers/pokemon_list_mapper.dart
@@ -1,13 +1,15 @@
+import 'package:pokedex/src/core/config/pokemon_types.dart';
 import 'package:pokedex/src/data/models/pokemon_list_model.dart';
 import 'package:pokedex/src/domain/entities/pokemon_list_entity.dart';
+import 'package:pokedex/src/shared/utils/string_util.dart';
 
 class PokemonListMapper {
   static PokemonListEntity fromEntity(PokemonListModel entity) {
     return PokemonListEntity(
       number: entity.id,
-      name: entity.name,
-      imageUrl: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png',
-      types: ['Grass', 'Poison'],
+      name: StringUtil.capitalizeFirst(entity.name),
+      imageUrl: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${entity.id}.png',
+      types: PokemonTypes.typesByPokemon[entity.name] ?? [],
     );
   }
 }

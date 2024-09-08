ee8236dc84afd1bf4383986be8c0248ffc7a1e24
CaioWF <contato.caioweliton@gmail.com>
2024-09-08 16:52:08 -0300

    feat: :sparkles: add provider on main

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:pokedex/src/core/config/app_config.dart';
+import 'package:pokedex/src/domain/usecases/fetch_pokemon_list_usecase.dart';
+import 'package:pokedex/src/presenter/home/home_controller.dart';
+import 'package:provider/provider.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:pokedex/src/data/repositories/pokemon_repository_impl.dart';
 import 'package:pokedex/src/presenter/home/home.dart';
 import 'package:pokedex/src/shared/theme/theme.dart';
 
 void main() {
-  runApp(const MyApp());
+  //repositories
+  final pokemonRepository = PokemonRepositoryImpl(apiBaseUrl: AppConfig.apiBaseUrl);
+  
+  //usecases
+  final fetchPokemonListUsecase = FetchPokemonListUsecase(pokemonRepository);
+
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => HomeController(fetchPokemonListUsecase),
+        ),
+      ],
+      child: const App(),
+    ),
+  );
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class App extends StatelessWidget {
+  const App({super.key});
 
   @override
   Widget build(BuildContext context) {

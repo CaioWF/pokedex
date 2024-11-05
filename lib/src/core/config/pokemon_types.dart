c0b55cf84161c7894f4e4a6cbebbbdec0b3b42ea
CaioWF <contato.caioweliton@gmail.com>
2024-11-04 23:37:46 -0300

    feat: :wrench: add pokemon types const

---
+++ lib/src/core/config/pokemon_types.dart
@@ -0,0 +1,41 @@
+class PokemonTypes {
+  static const String bug = 'bug';
+  static const String dark = 'dark';
+  static const String dragon = 'dragon';
+  static const String electric = 'electric';
+  static const String fairy = 'fairy';
+  static const String fighting = 'fighting';
+  static const String fire = 'fire';
+  static const String flying = 'flying';
+  static const String ghost = 'ghost';
+  static const String grass = 'grass';
+  static const String ground = 'ground';
+  static const String ice = 'ice';
+  static const String normal = 'normal';
+  static const String poison = 'poison';
+  static const String psychic = 'psychic';
+  static const String rock = 'rock';
+  static const String steel = 'steel';
+  static const String water = 'water';
+
+  static const List<String> all = [
+    bug,
+    dark,
+    dragon,
+    electric,
+    fairy,
+    fighting,
+    fire,
+    flying,
+    ghost,
+    grass,
+    ground,
+    ice,
+    normal,
+    poison,
+    psychic,
+    rock,
+    steel,
+    water,
+  ];
+}
